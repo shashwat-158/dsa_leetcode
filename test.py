d5fd3017577115f89885bf4c8a2945a63ff8739b
shashwatnarayan15@gmail.com <shashwatnarayan15@gmail.com>
2026-02-27 15:36:51 +0000

    updated boilerplate

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
+# --- IMPORTS (LEETCODE HANDLES THESE AUTOMATICALLY) ---
 from typing import List, Optional, Dict, Tuple
 from collections import defaultdict, deque, Counter
 import heapq
 import math
 
+# ---- COPY AND PASTE THIS BLOCK TO LEETCODE ----
 class Solution:
     def function_name(self, nums: List[int]) -> int:
         
         pass
+# ---- COPY AND PASTE THIS BLOCK TO LEETCODE ---
 
+# --- RUN THIS LOCALLY ---
 if __name__ == '__main__':
     sol = Solution()
     
