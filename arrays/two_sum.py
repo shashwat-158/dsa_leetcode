144de33d4b66b911259b29a78e0e914d7390c484
shashwatnarayan15@gmail.com <shashwatnarayan15@gmail.com>
2026-02-27 14:10:17 +0000

    starting

---
+++ arrays/two_sum.py
@@ -0,0 +1,22 @@
+from typing import List
+
+# --- PASTE THIS PART INTO LEETCODE ---
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash_map = {}
+        for i, num in enumerate(nums):
+            diff = target - num
+            if diff in hash_map:
+                return [hash_map[diff], i]
+            hash_map[num] = i
+        return []
+# -------------------------------------
+
+# --- RUN THIS LOCALLY IN VS CODE ---
+if __name__ == '__main__':
+    sol = Solution()
+    
+    # Test Case 1
+    nums = [2, 7, 11, 15]
+    target = 9
+    print(f"Output: {sol.twoSum(nums, target)}")
